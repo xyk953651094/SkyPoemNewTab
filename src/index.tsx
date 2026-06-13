6bf50e0cce650b2003039e0dab2ce974f10fbb34
待看云开 <xyk953651094@qq.com>
2026-06-13 11:27:08 +0800

    提升代码质量

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {ConfigProvider} from "antd";
+import zhCN from "antd/es/locale/zh_CN";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        <ConfigProvider locale={zhCN}>
+            <App/>
+        </ConfigProvider>
+    </React.StrictMode>
+);
